80e39051ecdf6cc0261aef0e136291798cb3225e
Zaeem <raozaeem2018@gmail.com>
2025-12-07 15:43:53 +0500

    copilot.vim replaced with copilot.lua with copilot-lsp and keymap config

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,12 +1,35 @@
 return {
   { "wakatime/vim-wakatime", lazy = false },
 
-  "github/copilot.vim",
-  config = function()
-    -- Set key binding for accepting suggestion
-    vim.g.copilot_no_tab_map = true
-    vim.api.nvim_set_keymap("i", "<C-g>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-  end,
+  {
+    "zbirenbaum/copilot.lua",
+
+    dependencies = {
+      "copilotlsp-nvim/copilot-lsp",
+    },
+
+    cmd = "Copilot",       -- Add 'cmd' for lazy loading
+    event = "InsertEnter", -- Trigger loading on 'InsertEnter'
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-g>", -- Map <C-g> to accept the suggestion
+          -- You can also remap other actions if needed
+          -- accept_word = false,
+          -- accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
+      -- Other Copilot options can be configured here
+    },
+    setup = function()
+      -- Set the global variable to disable the default Tab mapping
+      vim.g.copilot_no_tab_map = true
+    end,
+  },
 
   {
     "folke/todo-comments.nvim",
